ea5023f21f7282a045c5713dc3f378635e781657
adrianlievano <adrianlievano1993@gmail.com>
2019-03-15 23:05:35 -0700

    Fixed: Format issues and clearing grid each time user resubmits.

--- designs.js
+++ designs.js
@@ -1,29 +1,32 @@
 
 
+//Target the relevant HTML elements to pick the color and to control the size
+//of the grid
 
 const colorPicker = document.getElementById('colorPicker');
 const gridSize = document.getElementById('sizePicker');
 
 function makeGrid() {
-  const canvas = document.getElementById('pixelCanvas');
-  const gridHeight = document.getElementById('inputHeight').value;
-  const gridWidth = document.getElementById('inputWidth').value;
-
-//Builds grid
-  for (var i = 1; i <= gridHeight; i++){
-    const newRow = document.createElement('tr');
-    canvas.appendChild(newRow);
-    for (var j = 1; j <= gridWidth; j++){
-      const newCol = document.createElement('td');
-      canvas.appendChild(newCol);
-  newCol.addEventListener('click', function () {
-    event.target.style.backgroundColor = colorPicker.value;
-  })
+    const canvas = document.getElementById('pixelCanvas');
+    const gridHeight = document.getElementById('inputHeight').value;
+    const gridWidth = document.getElementById('inputWidth').value;
+    canvas.innerHTML = '';
+    //Builds grid and adds an event listener to each table data element
+    for (var i = 1; i <= gridHeight; i++) {
+        const newRow = document.createElement('tr');
+        canvas.appendChild(newRow);
+        for (var j = 1; j <= gridWidth; j++) {
+            const newCol = document.createElement('td');
+            canvas.appendChild(newCol);
+            newCol.addEventListener('click', function() {
+                event.target.style.backgroundColor = colorPicker.value;
+            })
+        }
     }
-  }
 }
 
-gridSize.addEventListener('submit', function (event){
-  event.preventDefault();
-  makeGrid();
+//Adds an event listener for when the user submits the form.
+gridSize.addEventListener('submit', function(event) {
+    event.preventDefault(); //prevents the form from being sent to server
+    makeGrid();
 });
